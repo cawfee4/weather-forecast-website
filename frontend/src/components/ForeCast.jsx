c94afab3c44503e3e7228eebd9836f44625ff7a9
unknown <cawfee4@gmail.com>
2024-06-16 21:24:52 +0700

    fix text color

--- frontend/src/components/ForeCast.jsx
+++ frontend/src/components/ForeCast.jsx
@@ -87,7 +87,7 @@ const ForeCast = ({ weatherData, day, handleSearch }) => {
       {!isEmptyObject(weatherData) && (
         <>
           <ForeCastBanner weatherData={weatherData} />
-          <Heading as="h1" size="lg" color="weather.gray">
+          <Heading as="h1" size="lg" color="black">
             {day}-Day Forecast
           </Heading>
           <Grid
